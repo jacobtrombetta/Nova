8d3436f5d4acd16c3fa3a54402d18f6cf1e6d2b0
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2025-02-06 13:40:17 -0500

    feat: add blitzar gpu support for bn256 msm

--- src/provider/bn256_grumpkin.rs
+++ src/provider/bn256_grumpkin.rs
@@ -51,8 +51,13 @@ impl Group for bn256::Point {
 }
 impl DlogGroup for bn256::Point {
   type AffineGroupElement = bn256::Affine;
+  #[cfg(not(feature = "blitzar"))]
   fn vartime_multiscalar_mul(scalars: &[Self::Scalar], bases: &[Self::AffineGroupElement]) -> Self {
     msm_best(scalars, bases)
+  }
+  #[cfg(feature = "blitzar")]
+  fn vartime_multiscalar_mul(scalars: &[Self::Scalar], bases: &[Self::AffineGroupElement]) -> Self {
+    super::blitzar::vartime_multiscalar_mul(scalars, bases)
   }
   fn affine(&self) -> Self::AffineGroupElement {
     self.to_affine()
